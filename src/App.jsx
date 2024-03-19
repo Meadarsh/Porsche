ca787d772eb00a5d3f4a3de447371f4ec767419c
meadarsh <itsadarsh33@gmail.com>
2024-03-19 15:25:40 +0530

    fef

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,61 @@
-import React, { useEffect } from 'react'
-import './styles.css'
-import './input.css'
-import './component/firstpstyle.css'
-import FirstPage from './component/firstPage'
-import Secondp from './component/secondp'
-import Thirdp from './component/Thirdp'
-import Fourthp from './component/fourthp'
-import Fifthp from './component/fifthp'
-import Sixthpage from './component/sixthpage'
-import Contact from './component/contact'
+import React, { useEffect, useState } from "react";
+import "./styles.css";
+import "./input.css";
+import "./component/firstpstyle.css";
+import FirstPage from "./component/firstPage";
+import Secondp from "./component/secondp";
+import Thirdp from "./component/Thirdp";
+import Fourthp from "./component/fourthp";
+import Fifthp from "./component/fifthp";
+import Sixthpage from "./component/sixthpage";
+import Contact from "./component/contact";
 import Lenis from "@studio-freight/lenis";
 import ScrollTrigger from "gsap/src/ScrollTrigger";
 import gsap from "gsap";
 
 const App = () => {
-  useEffect(()=>{
-    const lenis = new Lenis()
-    lenis.on('scroll', ScrollTrigger.update)
+  useEffect(() => {
+    const lenis = new Lenis();
+    lenis.on("scroll", ScrollTrigger.update);
+
+    gsap.ticker.add((time) => {
+      lenis.raf(time * 1000);
+    });
+    gsap.ticker.lagSmoothing(0);
+  });
+  const [isMobile, setIsMobile] = useState(true);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(!(window.innerWidth < 768));
+    };
+   
+     // Set initial state based on window width
+    window.addEventListener("resize", handleResize); // Listen for window resize events
+    handleResize();
+    return () => {
+      window.removeEventListener("resize", handleResize); // Cleanup event listener
+    };
     
-    gsap.ticker.add((time)=>{
-      lenis.raf(time * 1000)
-    })
-    gsap.ticker.lagSmoothing(0)
-    
-  })
-  
-  
+  }, []);
+
   return (
     <>
-   
-    <div className='firstpdiv'>
-    
-    <FirstPage/>
-       </div>
-       <Secondp/>
-       <Thirdp/>
-       <Fourthp/>
-       <Fifthp/>
-       <Sixthpage/>
-       <Contact/>
-      
-   
-       </>
-  )
-}
+      {isMobile ? <>
+          <div className="firstpdiv">
+            <FirstPage />
+          </div>
+          <Secondp />
+          <Thirdp />
+          <Fourthp />
+          <Fifthp />
+          <Sixthpage />
+          <Contact />
+        </>:
+        <div>This site is only available for desktop</div>       
+      }
+    </>
+  );
+};
 
-export default App
+export default App;
